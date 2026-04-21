8910ec535b1a891272e33da269b412ca2dcc9503
bromag <giuliano.romagnoli@hotmail.com>
2026-04-21 21:01:38 +0200

    updaten .sh

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo "=== Make scripts executable ==="
-sudo chmod +x /hacking-demo/scripts/ 
+sudo chmod +x ./scripts/ 
 
 echo "=== Run script 1: lamp.sh ==="
-sudo bash .hacking-demo/scripts/lamp.sh
+sudo bash ./scripts/lamp.sh
 
 echo "=== Run script 2: phpmyadmin.sh ==="
-sudo bash .hacking-demo/scripts/phpmyadmin.sh
+sudo bash ./scripts/phpmyadmin.sh
 
 echo "=== Run script 3: hacking_demo.sh ==="
-sudo bash .hacking-demo/scripts/hacking_demo.sh
+sudo bash ./scripts/hacking_demo.sh
 
 echo "=== Done ==="
